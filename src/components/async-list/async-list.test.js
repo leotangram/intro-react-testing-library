69fc0399181d53f740e8d3e6c81e5c05bf188082
Leonardo Omaña <leotangram@gmail.com>
2021-04-21 11:04:14 -0500

    Utilities for testing asynchrony

---
+++ src/components/async-list/async-list.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+
+import AsyncList from './async-list'
+
+test('show the food data', async () => {
+  render(<AsyncList />)
+
+  const hamburguer = await screen.findByText(/hamburger/i)
+
+  expect(hamburguer).toBeInTheDocument()
+
+  expect(await screen.findByText(/pizza/i)).toBeInTheDocument()
+  expect(await screen.findByText(/tacos/i)).toBeInTheDocument()
+})
